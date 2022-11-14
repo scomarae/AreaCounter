ddb28e7c1e7cea2c2e924d25a343ef5045b448a2
scomarae <78471996+scomarae@users.noreply.github.com>
2022-11-14 15:44:42 +0900

    Add files via upload

---
+++ Circle.cs
@@ -0,0 +1,12 @@
+﻿namespace ClassLibraryAreaCounter
+{
+    public class Circle
+    {
+        double rad;
+        public Circle(double rad)
+        {
+            this.rad = rad;
+        }
+        public void Print() => Console.WriteLine($"Площадь круга: {Math.Pow(rad, 2)*Math.PI}");
+    }
+}
---
+++ Triangle.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassLibraryAreaCounter
+{
+    public class Triangle
+    {
+        double a, b, c, p;
+
+
+        public Triangle(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.p = (a + b + c) / 2; ;
+        }
+
+        public void Print() => Console.WriteLine($"Площадь тругольника: {Math.Sqrt(p*(p-a)*(p-b)*(p-c))}");
+    }
+}
